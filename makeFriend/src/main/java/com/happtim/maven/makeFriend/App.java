1c0805cb2cc8d5df312e7d473a1e3d632661297c
Tim Ge <526821398@qq.com>
2016-09-10 00:00:08 +0800

    添加makeFriend 用于依赖案例

---
+++ makeFriend/src/main/java/com/happtim/maven/makeFriend/App.java
@@ -0,0 +1,13 @@
+package com.happtim.maven.makeFriend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
